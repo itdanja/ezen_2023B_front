597f40bff6e3e6494dd8a55ec5ddc1bd5ebbd97d
itdanja <itdanja@kakao.com>
2023-12-18 12:30:57 +0900

    20231218

--- day17/step1.js
+++ day17/step1.js
@@ -10,51 +10,45 @@
             // - 입력 데이터에는 구분 문자가 포함되면 안된다.
             // - 단점 : 분해 하고 분해후 순서번호 기억*.
 
-const boardArray  = [ ]
+// 배열을 상수에 선언. // 그 배열은 '작성자/비밀번호/제목/내용' 을 하나의 문자열로 하나씩 저장.
+const boardArray  = [ '유재석/1234/제목1/내용1' , '강호동/5678/제목2/내용2'  ] 
 
-// 1. [C]글쓰기 [ 글쓰기 버튼을 클릭했을때. ]
-function 글쓰기(){  // 매개변수 : X // 리턴값 : X
-    // 1. HTML로 부터 입력받은 값 가져오기 
-    const writer = document.querySelector('#writer').value;
-    const passowrd = document.querySelector('#passowrd').value;
-    const title = document.querySelector('#title').value;
-    const content = document.querySelector('#content').value;
-    // 2. 배열에 저장하기
-    const board = `${writer}/${passowrd}/${title}/${content}`;  console.log( board );
-    boardArray.push( board );   console.log( boardArray );
-    // 3. 배열에 HTML에 출력하기
+function 글쓰기(){  // HTML에 있는 (글쓰기버튼)onclik 했을때 실행되는 함수.
+
+    const writer = document.querySelector('#writer').value;         // (#writer)로 설정된 id에 입력된 값 JS로 가져온다.
+    const passowrd = document.querySelector('#passowrd').value;     // (#passowrd)로 설정된 id에 입력된 값 JS로 가져온다.
+    const title = document.querySelector('#title').value;           // (#title)로 설정된 id에 입력된 값 JS로 가져온다.
+    const content = document.querySelector('#content').value;       // (#content)로 설정된 id에 입력된 값 JS로 가져온다.
+
+    const board = `${writer}/${passowrd}/${title}/${content}`;      //  board는 '작성자/비밀번호/제목/내용' 하나의 문자열로 만들고
+    boardArray.push( board );                                       //  합칩문자열(board)가 배열에 저장된다.
+
     모든글출력();
-    // 
-    document.querySelector('#writer').value = ``;
-    document.querySelector('#passowrd').value = ``;
-    document.querySelector('#title').value = ``;
-    document.querySelector('#content').value = ``;
-    return;
+
+    document.querySelector('#writer').value = ``;                   // (#writer)로 설정된 id에 입력된 값 을 초기화한다.
+    document.querySelector('#passowrd').value = ``;                 // (#passowrd)로 설정된 id에 입력된 값 을 초기화한다.
+    document.querySelector('#title').value = ``;                    // (#title)로 설정된 id에 입력된 값 을 초기화한다.
+    document.querySelector('#content').value = ``;                  // (#content)로 설정된 id에 입력된 값 을 초기화한다.
+    return;                                                         //  함수종료.      
+
 }
-// 2-1. [R]모든글출력 [ 1.js가 열렸을때 2.글쓰기/글수정/글삭제 처리 할때마다 ]
-function 모든글출력(){ // 매개변수 : X // 리턴값 : X
-    const tableBody = document.querySelector('table>tbody');
-    let html = ``;  
-    for( let i = 0 ; i<boardArray.length ; i++ ){
-        console.log( boardArray[i] );
-        // 저장할떄 4가지 속성/상태 값들을 "/" 기준으로 하나로 묶었으니까. 다시 "/" 기준으로 분리[ split() ] 하자.
-        console.log( boardArray[i].split("/") );
-        html += `<tr onclick="개별글출력( ${i} )" >
-                    <th>${ i }</th>   
-                    <th>${ boardArray[i].split("/")[2] }</th>  
-                    <th>${ boardArray[i].split("/")[0] }</th>
-                </tr>`
+
+function 모든글출력(){ // [글쓰기,글수정,글삭제 할떄마다] 배열에 있는 데이터를 HTML화 해서 HTML 출력하는 함수.
+    const tableBody = document.querySelector('table>tbody');        // HTML 안에 있는 tbody 마크업을 tableBody상수에 저장 
+    let html = ``;  // html문법 저장할 변수 선언
+    for( let i = 0 ; i<boardArray.length ; i++ ){                   // 배열에 있는 모든 요소를 처음부터 끝까지 반복.
+        html += `<tr onclick="개별글출력( ${i} )" >                  // tr(행 시작) 온클릭했을때 개별글출력() 함수 실행 -> 매개변수로 인덱스(보고자하는 개별게시물의 식별번호)를 전달.
+                    <th>${ i }</th>                                 // 인덱스를 게시물번호 처름 사용.
+                    <th>${ boardArray[i].split("/")[2] }</th>       // 해당 인덱스 요소의 문자열을 '/' 분리후 3번째 데이터 출력 
+                    <th>${ boardArray[i].split("/")[0] }</th>       // 해당 인덱스 요소의 문자열을 '/' 분리후 0번째 데이터 출력 
+                </tr>`                                              // (행 종료) .
     }
-    tableBody.innerHTML = html;
-    return;
+    tableBody.innerHTML = html;                                     // tableBody에 마크업사이(innerHTML)에 html를 대입한다.
+    return;                                                         // 함수종료.
 }
-// 2-2 [R]개별글출력  [ 해당 글을 선택했을때. ]
-function 개별글출력( index ){ // 매개변수 : (대상)보고자하는 식별번호 // 리턴값 : X
-    console.log( index );
+
+function 개별글출력( index ){
     const boardBox = document.querySelector('#boardBox');
-    // - replaceAll( '교체할문자열" , "새로운문자열" ) : 특정 문자열 치환 함수.
-    // - \n을 <br/> 변경 : <textarea> 에서 줄바꿈처리하면 \n으로 저장되고 HTML 출력할때 \n 대신에 <br/> 치환
-    console.log( boardArray[index].split("/")[3].replaceAll( '\n' , '<br/>') )
     let html = `<div> 작성자 : ${ boardArray[index].split("/")[0] } </div>
                 <div> 제목 : ${ boardArray[index].split("/")[2] } </div>
                 <div> 내용 : ${ boardArray[index].split("/")[3].replaceAll( '\n' , '<br/>') } </div>
@@ -64,45 +58,31 @@ function 개별글출력( index ){ // 매개변수 : (대상)보고자하는 식
     boardBox.innerHTML = html;
     return;
 }
-// 3. [U]글수정 [ 글수정 버튼을 클릭했을때. ]
-function 글수정( index ){ // 매개변수 : (대상)수정할 식별번호 // 리턴값 : X
-    console.log( index );
-    // 1. 검증[유효성검사] - 패스워드를 입력받아.. 해당 게시물의 패스워드와 동일하면.
+
+function 글수정( index ){ 
     const checkPw = prompt('비밀번호');
-    if( checkPw == boardArray[index].split("/")[1] ){  // 만약에 입력받은 패스워드와 게시물의 패스워드가 일치하면
-        // 수정할 값들을 입력받아.
+    if( checkPw == boardArray[index].split("/")[1] ){ 
         const updateTitle = prompt('수정할 제목');
         const updateContent = prompt('수정할 내용');
-        // 수정처리 [ 기존데이터와 새로운데이터 다시 합쳐서 수정.]
         boardArray[index] = `${boardArray[index].split('/')[0]}/${boardArray[index].split('/')[1] }/${updateTitle}/${updateContent}`;
         alert('수정 성공');
-        // 새로고침.
-        모든글출력();  개별글출력(index);
+        모든글출력();  
+        개별글출력(index);
     }else{
         alert('패스워드가 다릅니다. 수정 불가.')
     }
     return;
 }
-// 4. [D]글삭제 [ 글삭제 버튼을 클릭했을때.]
-function 글삭제( index ){ // 매개변수 : (대상)삭제할 식별번호  // 리턴값 : X
-    console.log( index );
+
+function 글삭제( index ){ 
     const checkPw = prompt('비밀번호');
     if( checkPw == boardArray[index].split("/")[1] ){
-        // 삭제처리
         boardArray.splice( index , 1 );
         alert('삭제 성공');
-        // 새로고침.
-        모든글출력(); 
-        document.querySelector('#boardBox').innerHTML = ``; // 개별글보기 구역 초기화.
+        모든글출력();
+        document.querySelector('#boardBox').innerHTML = ``;
     }else{
         alert('패스워드가 다릅니다. 삭제 불가.');
     }
     return;
 }
-/*
-    함수 만들때 고민할점.
-        1. 언제 실행하는 함수인지?
-        2. 함수 안으로 넣을 매개변수?
-        3. 함수 밖으로 반환할 값?
-        4. 무엇을 실행 하는지?
-*/
