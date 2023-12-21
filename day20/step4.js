63e5ed82d182a4b79b024c8d3907ffd1c57148c1
itdanja <itdanja@kakao.com>
2023-12-21 16:26:32 +0900

    20231221

--- day20/step4.js
+++ day20/step4.js
@@ -11,8 +11,12 @@ function 등록( ){ console.log('등록( )함수');
     }; console.log( member );
         // 2. 객체를 배열에 저장 . 전역변수가 아닌. 브라우저에 저장.
         // localStorage 에 입력받은 객체를 저장한다. [ JSON.stringify(  ) : 객체를-->JSON형식의 문자열]
+    // 기존 배열 호출     
+    let memberArray = JSON.parse( localStorage.getItem('memberArray') ) ;     
+    console.log( memberArray );
+        // - 만약에 기존배열을 호출할때 없으면=null. 있으면 = 배열
+    if( memberArray == null ){ memberArray = [ ] } // 배열을 새로 만들어준다. 
     
-    const memberArray = JSON.parse( localStorage.getItem('memberArray') ) ;     // 기존 배열 호출
     memberArray.push( member ); // 방금 가입된 회원객체 배열에 추가 
     localStorage.setItem( 'memberArray' , JSON.stringify( memberArray )  );    
     // 3. 출력 [x]
